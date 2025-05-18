1410e17573d5c89d8413a1fabe892b30200065ff
kabboghosh <kabbo25@gmail.com>
2025-05-19 00:52:08 +0600

    refactor: update data structure to use flattened travel plan
    Refactor the data handling in App.jsx and PlaceTree.jsx to utilize a flattened travel plan structure. This change improves maintainability by centralizing the data structure and simplifies the rendering logic. The key generation for list items has also been updated to ensure uniqueness.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import PlaceTree from "./PlaceTree.jsx";
 import {initialTravelPlan as places} from "./places.jsx";
+import {flattenedTravelPlan} from "./flatPlaces.js";
 
 function App() {
-   const childPlaces = places.childPlaces;
+   const childPlaces = flattenedTravelPlan[0].childIds;
   return (
     <>
         <ol className={'bg-green-100 m-20'}>
@@ -12,7 +13,7 @@ function App() {
             childPlaces.map(place => (
                 <div className={'mb-5'}>
 
-                    <PlaceTree place={place}  key={place.id}/>
+                    <PlaceTree place={place}  key={place+'$'}/>
                 </div>
             ))
         }
--- src/PlaceTree.jsx
+++ src/PlaceTree.jsx
@@ -1,9 +1,13 @@
+import {flattenedTravelPlan} from "./flatPlaces.js";
+
 const PlaceTree = ({place}) => {
+    const currentNode = flattenedTravelPlan[place];
     return (
         <div className={'border border-black-500 mb-3 px-10'}>
-            <li className={'text-4xl mb-3'}>{place.title}</li>
-                {place.childPlaces.map(childPlace => (
-                    <PlaceTree place={childPlace} key={childPlace.id}/>
+
+            <li className={'text-4xl mb-3'}>{currentNode.title}</li>
+                {currentNode.childIds.map(childPlace => (
+                    <PlaceTree place={childPlace} key={flattenedTravelPlan[childPlace].title+'$'}/>
                 ))}
 
         </div>
